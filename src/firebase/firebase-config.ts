1714e077dfaebda6168fb60a65f4649e504e4ca4
Bocchi The Python <benjaigancioreyes56@gmail.com>
2024-10-06 01:05:43 -0300

    Actualizar firebase-config.ts

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -12,15 +12,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; // Importa Firebase Storage
 
 // Tu configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBKBBp8GYB_G05kiLbN813ondwBrQe9CF4",
-  authDomain: "data-base-uber-eats.firebaseapp.com",
-  databaseURL: "https://data-base-uber-eats-default-rtdb.firebaseio.com",
-  projectId: "data-base-uber-eats",
-  storageBucket: "data-base-uber-eats.appspot.com",
-  messagingSenderId: "948116538428",
-  appId: "1:948116538428:web:b7d2cd4b23d13b4bbfaf39",
-  measurementId: "G-BE23FLZF7Z",
+
 };
 
 // Inicializa Firebase
